f7fed477b9e1067ed059345adefd6a15621bae92
Mike Hay <mhay@protonmail.com>
2020-10-07 08:28:11 -0700

    jekyll-include-cache

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -2,30 +2,30 @@ var store = [{
         "title": "Dropout and early stopping with Adagrad",
         "excerpt":"I was experimenting with applying variational dropout, using multiplicative Gaussian output noising, to primal generalized Gaussian process models. I’m going to show an example of Gaussian process classification with dropout, and show that it gives nearly the same performance as early stopping with Adagrad. This is because of some surprising...","categories": ["jekyll","update"],
         "tags": [],
-        "url": "/jekyll/update/2017/05/12/variational-glm.html",
+        "url": "http://localhost:4000/jekyll/update/2017/05/12/variational-glm.html",
         "teaser": null
       },{
         "title": "Turns out that predicting forest fires from scant met data is hard",
         "excerpt":"Turns out that predicting forest fire size from scant met data is hard.&#194;&#182; Every year, there are many wildfires in my native Washington (state, obviously). Many aren't a big deal. They spread anemically and predictibly, and fizzle out in the autumn. Fires like that are a good thing. It's a...","categories": ["jekyll","update"],
         "tags": [],
-        "url": "/jekyll/update/2017/07/25/forest_fires.html",
+        "url": "http://localhost:4000/jekyll/update/2017/07/25/forest_fires.html",
         "teaser": null
       },{
         "title": "P-values do not tell you what you probably think they do",
         "excerpt":"(This content first appeared on openchannels.org) Calling everything with p &lt; 0.05 &quot;significant&quot; is just plain wrong The practice of statistics in the sciences often takes the form of drawing scientific conclusions from cargo-cult application of inappropriate or outdated statistical methods, often to the exclusion of prior evidence or plausibility....","categories": ["jekyll","update"],
         "tags": [],
-        "url": "/jekyll/update/2018/06/19/p-values.html",
+        "url": "http://localhost:4000/jekyll/update/2018/06/19/p-values.html",
         "teaser": null
       },{
         "title": "P-values part II: How a p<0.05 result can have more than a 50% chance of being wrong",
         "excerpt":"(This content first appeared on openchannels.org) In the last post, I described some common misconceptions and problems with the use of null-hypothesis significance tests and P-values. In this post, I&#39;ll show more common ways that P-values are often misapplied, including how a significant result under $\\alpha = 0.05$ can have...","categories": ["jekyll","update"],
         "tags": [],
-        "url": "/jekyll/update/2018/06/28/p-values.html",
+        "url": "http://localhost:4000/jekyll/update/2018/06/28/p-values.html",
         "teaser": null
       },{
         "title": "Bayesian statistics: Common sense formalized",
         "excerpt":"(This content first appeared on openchannels.org) In the last posts (first, second), I outlined a number of common errors in the usage and interpretation of P-values. Due to the base-rate fallacy or the multiple comparisons problem, the significance level $ \\alpha $ of a null-hypothesis significance test can easily be...","categories": ["jekyll","update"],
         "tags": [],
-        "url": "/jekyll/update/2018/07/26/bayesian.html",
+        "url": "http://localhost:4000/jekyll/update/2018/07/26/bayesian.html",
         "teaser": null
       }]
